2ba044cbb856f2a070b968776576d4ba8c120e09
안홍범 <hongbeom7877@naver.com>
2024-07-08 09:56:25 +0900

    first

---
+++ T82/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T82
+//
+//  Created by 안홍범 on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T82/T82App.swift
@@ -0,0 +1,17 @@
+//
+//  T82App.swift
+//  T82
+//
+//  Created by 안홍범 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct T82App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
