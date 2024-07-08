2c572c13fba5d7eca47e7d7a890e07f897a18195
안홍범 <hongbeom7877@naver.com>
2024-07-08 12:32:04 +0900

    test2

--- T82/ContentView.swift
+++ T82/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world123!")
+            Text("Hello, world222!")
         }
         .padding()
     }
