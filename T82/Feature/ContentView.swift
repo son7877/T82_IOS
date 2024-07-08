7d54ee3bd7da729d8731bab4fa926ae305395674
안홍범 <hongbeom7877@naver.com>
2024-07-08 15:49:45 +0900

    [feat] 프로젝트 구조 설정 및 LauchScreen 생성

--- T82/Feature/ContentView.swift
+++ T82/Feature/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world222!")
+            Text("Verson 1.0.0")
         }
         .padding()
     }
